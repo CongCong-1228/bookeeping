46c08e3bb0cb1b1512a09aa1917cf3302200fe2e
congcong1228 <fivesun1228@163.com>
2022-04-17 22:30:00 +0800

    import styled-components and node-sass@npm:dart-sass import react-router-dom

---
+++ src/components/button.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  border: none;
+  color: red;
+  padding: 8px;
+
+  &:hover {
+    background: green;
+    color: #fff;
+    cursor: pointer;
+  }
+`
+
+export default Button;
